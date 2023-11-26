d33d0eedd85f8eb714e124ae54f787a4a5e02891
Nandini Jaiswal <77235640+Yeagerist-Nandini@users.noreply.github.com>
2023-11-26 19:21:05 +0530

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,21 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Token{
-    string public name="HardHat TOken";
-    string public symbol="HHT";
-    uint public totalSupply=1000;
+import "../node_modules/hardhat/console.sol";
 
+contract Token{
+    string public name="HardHat Token";
+    string public symbol="HHT";
+    uint public totalSupply=10000;
     address public owner;
     mapping(address=>uint) balance;
 
     constructor(){
-        balance[msg.sender]=totalSupply;
         owner=msg.sender;
+        balance[msg.sender]=totalSupply;
     }
 
-    function tranfer(address to,uint amount) external{
-        require(amount<=balance[msg.sender],"Not enough Tokens");
+    function transfer(address to,uint amount) public {
+        console.log("**Sender balance is %s tokens",balance[msg.sender]);
+        console.log(
+        "**Transferring from %s to %s %s tokens",
+        msg.sender,
+        to,
+        amount
+    );
+
+        require(balance[msg.sender]>=amount,"Not enough tokens");
         balance[msg.sender]-=amount;
         balance[to]+=amount;
     }
